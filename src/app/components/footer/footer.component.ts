4f7ee80db22d627602f56a29e6dc7a09e0e02768
webking0121 <bestwebdev010@gmail.com>
2020-11-25 04:04:07 +0800

    before the boss sleep

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostListener, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  width: number = 1240;
   constructor() { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.width = event.target.innerWidth;
+  }
 
   ngOnInit(): void {
   }
