32a7b0f8af722445d9705c984c94125a347b6eb6
webking0121 <bestwebdev010@gmail.com>
2020-11-25 12:45:49 +0800

    add rellax

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var Rellax : any;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var rellax = new Rellax('.rellax');
   }
 
 }
